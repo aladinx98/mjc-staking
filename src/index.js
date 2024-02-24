49aafe77071f87cf8db56f85630d59e13fc91e79
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-02-24 11:22:06 +0530

    mjc comit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+import { ChainId } from "@thirdweb-dev/sdk";
 
+const chainId = ChainId.BinanceSmartChainMainnet;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+   <ThirdwebProvider
+      supportedWallets={[metamaskWallet(), coinbaseWallet(), walletConnect()]}
+      activeChain={56}
+      clientId="c253ee2f378ff7eade282c568778ff7c"
+    >
+     <App />
+    </ThirdwebProvider>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  // other webpack configurations...
+
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "zlib": require.resolve("browserify-zlib"),
+      "stream": require.resolve("stream-browserify"),
+      "assert": require.resolve("assert/"),
+      "url": require.resolve("url/")
+    },
+  },
+};
